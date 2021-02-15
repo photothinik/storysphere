0b6005f528dc153e268b5228fe975a48c2318998
photothinik <53952128+photothinik@users.noreply.github.com>
2021-02-14 22:50:49 -0500

    Initial commit

---
+++ src/main/java/org/photothinik/storysphere/StorysphereApplication.java
@@ -0,0 +1,13 @@
+package org.photothinik.storysphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorysphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorysphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/photothinik/storysphere/StorysphereApplicationTests.java
@@ -0,0 +1,13 @@
+package org.photothinik.storysphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorysphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
